8de1b456b79ebef4568384dc95df5531db3c4368
Lucas Buzioli Fioravanti Palmieri <50854891+lbfpalmieri@users.noreply.github.com>
2024-07-08 23:55:24 -0300

    FinalmenteFinalizado
    Finalizamos o CRUD com sucesso com uma pequena ajudinha do curso obviamente + Copilot for Student !!! Ficou muito legal esse projeto e vai ser vir de base para os próximos projetinhos !

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Main from '../template/Main'
+import './UserCrud.css'
 
 const headerProps = {
     icon: 'user',
@@ -7,10 +9,184 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends Component {
+    state = { ...initialState }
+
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data })
+        })
+    }
+
+    // Inicializar a lista
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    // Incluir
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list })
+            })
+    }
+
+    // Atualizar a lista com os dados do usuário incluído
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
+    // Atualizar a lista com os dados do usuário excluído ou alterado
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    rederForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..." />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o email..." />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button
+                            className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+                        <button
+                            className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Limpar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    load(user) {
+        this.setState({ user })
+    }
+
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.state.list.filter(u => u !== user)
+            this.setState({ list })
+        })
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.list.map(user => {
+                        return (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button
+                                        className="btn btn-warning"
+                                        onClick={() => this.load(user)}>
+                                        <i className="fa fa-pencil"></i>
+                                    </button>
+                                    <button
+                                        className="btn btn-danger ml-2"
+                                        onClick={() => this.remove(user)}>
+                                        <i className="fa fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows() {
+        return this.state.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button
+                            className="btn btn-warning"
+                            onClick={() => this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button
+                            className="btn btn-danger ml-2"
+                            onClick={() => this.remove(user)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    // Atualizar a lista
     render(){
         return(
-            <Main {...headerProps}>Cadastro de Usuário</Main>       
+            <Main {...headerProps}>
+                <h3 className="titulo-cadastro">Cadastro De Usuário</h3>
+                <div className="mb-3"></div> {/* Espaçamento entre o título e o formulário */}
+                {this.rederForm()}
+                <div className="mb-3"></div> {/* Espaçamento entre o formulário e a tabela */}
+                {this.renderTable()}
+            </Main>     
         )
     }
 }
